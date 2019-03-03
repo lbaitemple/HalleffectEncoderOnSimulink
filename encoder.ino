1cdc0ef230782f4dcd7ea913b4d6c97631850405
lbaitemple <lbai@temple.edu>
2019-03-03 10:06:29 -0500

    Add files via upload

--- encoder.ino
+++ encoder.ino
@@ -1,48 +1,75 @@
+  int encoderA = 3;
+  int encoderB = 2;
+  int pulsesChanged = 0;
+int pulses;    //output counter
+int counter=0; //output counter – need to be reset each time when RPM is calculated
+int passedtime;
+double rpm;
 
-int pulses;                              //Output pulses.
-int encoderA = 3;
-int encoderB = 2;
-int pulsesChanged = 0;
+  
 
-void A_CHANGE() 
-{ 
-  //Interrupt function to read the x2 pulses of the encoder.
-  if ( digitalRead(encoderB) == 0 ) {
-    if ( digitalRead(encoderA) == 0 ) {
-      // A fell, B is low
-      pulses--; // Moving forward
+  void A_CHANGE() 
+  { 
+    //Interrupt function to read the x2 pulses of the encoder.
+    if ( digitalRead(encoderB) == 0 ) {
+      if ( digitalRead(encoderA) == 0 ) {
+        // A fell, B is low
+        pulses--; // Moving forward
+      counter--;
+      } else {
+        // A rose, B is high
+        pulses++; // Moving reverse
+      counter++;
+      }
     } else {
-      // A rose, B is high
-      pulses++; // Moving reverse
-    }
-  } else {
-    if ( digitalRead(encoderA) == 0 ) {
+      if ( digitalRead(encoderA) == 0 ) {
+  
 
-
-      pulses++; // Moving reverse
-    } else {
-      // A rose, B is low
-      pulses--; // Moving forward
+  
+        pulses++; // Moving reverse
+      counter++;
+      } else {
+        // A rose, B is low
+        pulses--; // Moving forward
+      counter--;
+      }
     }
+    pulsesChanged = 1;
   }
-  pulsesChanged = 1;
-}
+  
 
-void setup() {
-  Serial.begin(9600);
-  pinMode(encoderA, INPUT);
-  pinMode(encoderB, INPUT);
+  void setup() {
+    Serial.begin(9600);
+    pinMode(encoderA, INPUT);
+    pinMode(encoderB, INPUT);
+  counter=0;
+  passedtime=0;
+    attachInterrupt(0, A_CHANGE, CHANGE);
+  
+
+  
+
+  }//setup
+  
+
+  void loop() {
+  delay(1000); //update RPM every second 
+  detachInterrupt(0);
+  rpm = 60.0*1000/(millis()-passedtime)*counter/816;
+  passedtime= millis();
+  counter =0; //reset counter
   attachInterrupt(0, A_CHANGE, CHANGE);
+    if (pulsesChanged != 0) {
+      pulsesChanged = 0;
+      Serial.print("Encoder Count: ");
+  Serial.print(pulses);
+Serial.print("RPM Reading: ");
+Serial.println(rpm);
 
+    }
+  
 
-}//setup
+  
 
-void loop() {
-  if (pulsesChanged != 0) {
-    pulsesChanged = 0;
-    Serial.print("Encoder Count ");
-    Serial.println(pulses);
   }
 
-
-}
